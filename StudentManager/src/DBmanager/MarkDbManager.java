f788fd97c526fa7b1c1eab620a3ef8792fad64da
Ahmed Qazi <90492155+Ahmadqazi584@users.noreply.github.com>
2023-08-24 12:40:28 -0700

    Update StudentCourseDbManager.java

--- StudentManager/src/DBmanager/MarkDbManager.java
+++ StudentManager/src/DBmanager/MarkDbManager.java
@@ -13,17 +13,17 @@ import model.Course;
 import model.Marks;
 import model.Student;
 
-public class MarkDbManager {
+public class StudentCourseDbManager {
 
-    private final String INSERT_MARKS_RECORD = "INSERT INTO `dbs11786303`.`marks` (`student_id`, `course_id`, `mark`) VALUES (?, ?, ?);";
-    private final String UPDATE_MARKS = "UPDATE `marks` SET `mark` = ? WHERE `student_id` = ? AND `course_id` = ?";
+    private final String INSERT_STUDENT_COURSE_RECORD = "INSERT INTO `dbs11786303`.`marks` (`student_id`, `course_id`, `mark`) VALUES (?, ?, ?);";
+    private final String UPDATE_STUDENT_COURSE = "UPDATE `marks` SET `mark` = ? WHERE `student_id` = ? AND `course_id` = ?";
     private final String GET_STUDENT_COURSES = """
                                                SELECT s.student_id, s.name AS student_name, c.course_id, c.crse_name, m.mark
                                                FROM student s
                                                INNER JOIN marks m ON m.student_id = s.student_id
                                                INNER JOIN course c ON c.course_id = m.course_id""";
 
-    public void addMarks(Marks marks) {
+    public void addStudentCourse(Marks marks) {
         try {
             Connection connect = dbconnection.getconnection();
             PreparedStatement ps = connect.prepareStatement(INSERT_MARKS_RECORD);
